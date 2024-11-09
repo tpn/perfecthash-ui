0a0cf41f0ce014067b43f8353b4b45dc136194a1
Trent Nelson <trent@trent.me>
2024-11-08 20:43:34 -0800

    Fix base.

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+    base: '/perfecthash-ui/',
     plugins: [react()],
     server: {
         port: 3005,
